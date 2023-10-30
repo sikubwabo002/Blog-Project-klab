980cd60eb12beac6d0400e5cc3ebe7e492dd9a47
Sikubwabo002 <sosthenesikubwabo@gmail.com>
2023-10-30 16:40:07 +0200

    navbar done

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import { Link, useNavigate } from "react-router-dom";
-import { HiMenu } from "react-icons/Hi";
+import { Link } from "react-router-dom";
+import { HiMenu } from "react-icons/hi";
 
 export const Navbar = () => {
   return (
