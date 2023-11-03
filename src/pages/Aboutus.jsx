f2d9b838c1c54177aee7b53ca7876f0981899152
Sikubwabo002 <sosthenesikubwabo@gmail.com>
2023-11-03 18:20:30 +0200

    done with danny

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
-// import { Footer } from '../component/Footer'
-
-// import { Navbar } from './Navbar'
+import React from "react";
 
 const Aboutus = () => {
   return (
     <div>
-    <div className='about-us'>
-      {/* <Navbar/> */}
-        <p className='about-heading'>Abouts us</p>
-        <p className='about-description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod saepe cumque omnis sit possimus maxime dolorum rem odio magnam ducimus dolores voluptatem velit, placeat numquam ea assumenda aspernatur tenetur. Eos. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint modi omnis consectetur est non cum voluptatem repellendus dolor quia praesentium, illo quasi quos labore fuga optio dignissimos. Adipisci, possimus sit!</p>
-        <p className='about-description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod saepe cumque omnis sit possimus maxime dolorum rem odio magnam ducimus dolores voluptatem velit, placeat numquam ea assumenda aspernatur tenetur. Eos. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint modi omnis consectetur est non cum voluptatem repellendus dolor quia praesentium, illo quasi quos labore fuga optio dignissimos. Adipisci, possimus sit!</p>
-        <p className='about-description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod saepe cumque omnis sit possimus maxime dolorum rem odio magnam ducimus dolores voluptatem velit, placeat numquam ea assumenda aspernatur tenetur. Eos. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint modi omnis consectetur est non cum voluptatem repellendus dolor quia praesentium, illo quasi quos labore fuga optio dignissimos. Adipisci, possimus sit!</p>
-      
+      <div className="about-us">
+        <p className="about-heading">Abouts us</p>
+        <p className="about-description">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod saepe
+          cumque omnis sit possimus maxime dolorum rem odio magnam ducimus
+          dolores voluptatem velit, placeat numquam ea assumenda aspernatur
+          tenetur. Eos. Lorem ipsum, dolor sit amet consectetur adipisicing
+          elit. Sint modi omnis consectetur est non cum voluptatem repellendus
+          dolor quia praesentium, illo quasi quos labore fuga optio dignissimos.
+          Adipisci, possimus sit!
+        </p>
+        <p className="about-description">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod saepe
+          cumque omnis sit possimus maxime dolorum rem odio magnam ducimus
+          dolores voluptatem velit, placeat numquam ea assumenda aspernatur
+          tenetur. Eos. Lorem ipsum, dolor sit amet consectetur adipisicing
+          elit. Sint modi omnis consectetur est non cum voluptatem repellendus
+          dolor quia praesentium, illo quasi quos labore fuga optio dignissimos.
+          Adipisci, possimus sit!
+        </p>
+        <p className="about-description">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod saepe
+          cumque omnis sit possimus maxime dolorum rem odio magnam ducimus
+          dolores voluptatem velit, placeat numquam ea assumenda aspernatur
+          tenetur. Eos. Lorem ipsum, dolor sit amet consectetur adipisicing
+          elit. Sint modi omnis consectetur est non cum voluptatem repellendus
+          dolor quia praesentium, illo quasi quos labore fuga optio dignissimos.
+          Adipisci, possimus sit!
+        </p>
+      </div>
     </div>
-        
-    {/* <Footer/> */}
-    </div>
+  );
+};
 
-  
-    
-  )
-}
-
-export default Aboutus
+export default Aboutus;
